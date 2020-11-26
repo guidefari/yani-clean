9710cda9ea07e61589ffddf187a39490ff26341f
GT Fari <guideg6@gmail.com>
2020-11-26 22:41:55 +0200

    how it works section done

--- components/Home-page/Hero.jsx
+++ components/Home-page/Hero.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Hero = () => {
     return (
-        <div className='relative flex items-start justify-center mx-3 sm:items-center min-h-md md:bg-blue-shadowTwo md:py-10'>
+        <div className='relative flex items-start justify-center mx-3 sm:items-center min-h-sm md:bg-blue-shadowTwo lg:py-10'>
             <div className='absolute flex items-center justify-center md:static md:w-1/2'>
                 <div className='relative lg:pl-10'>
                     <Image
@@ -19,8 +19,8 @@ const Hero = () => {
                 </div>
             </div>
             <div className='absolute my-auto md:w-1/2 md:static'>
-                <main className='flex flex-col items-end justify-center pt-2 pr-5 md:pr-0 lg:pr-9 xl:pr-10 lg:pt-16 text-white-primary'>
-                    <h1 className='pb-4 text-2xl text-right lg:pb-10 md:text-4xl lg:text-5xl xl:text-6xl font-heading md:text-black-heading'>Keeping your home <br/> clean & clutter free</h1>
+                <main className='flex flex-col items-end justify-center pt-2 pr-5 md:pr-0 lg:pr-9 xl:pr-10 lg:pt-8 xl:pt-16 text-white-primary'>
+                    <h1 className='pb-4 text-2xl text-right xl:pb-10 md:text-4xl lg:text-5xl xl:text-6xl font-heading md:text-black-heading'>Keeping your home <br/> clean & clutter free</h1>
                     <p className='max-w-xl pb-6 text-sm text-right md:text-base lg:text-xl md:max-w-4xl md:text-black-BoBody'>Lorem ipsum dolor sit amet consectetur adipisicipudiandae, nihil dicta.Lore dignissimos porro. Odit enim expedita dolores, velit porro in cumque accusamus.</p>
                     <Link href='/book'>
                         <button className="inline-flex items-center px-3 py-1 text-base font-semibold border-0 rounded text-white-primary bg-blue-primary focus:outline-none hover:bg-blue-secondary ">
--- components/Home-page/HowItWorks.jsx
+++ components/Home-page/HowItWorks.jsx
@@ -2,36 +2,45 @@ import { Info } from "react-feather"
 
 const HowItWorks = () => {
     return (
-        <div className='transform -translate-y-10 bg-gradient-to-b from-white-true to-white-primary mx-14'>
-            <div className='flex items-center justify-center py-16 space-x-3 '>
-                <Info className='inline'/>
-                <h1 className='text-5xl font-heading text-black-heading'>How it works</h1>
+        <div className='transform -translate-y-10 bg-gradient-to-b from-white-true to-white-primary xl:mx-12 mx-14'>
+            <div className='flex items-center justify-center py-12 space-x-3 sm:py-16 '>
+                <div className='flex items-center justify-center w-8 h-8 rounded-full bg-blue-shadowTwo'>
+                    <Info className='inline text-blue-primary'/>
+                </div>
+                <h1 className='text-xl md:text-3xl lg:text-5xl font-heading text-black-heading'>How it works</h1>
             </div>
-            <section className='relative flex items-center justify-center w-full px-32 space-x-10'>
-                <div className='z-10 flex flex-col w-4/12 px-5 py-4 border-2 border-gray-400 shadow-md shadow-lg bg-white-true'>
+            <section className='relative flex flex-col items-center justify-center w-full px-5 space-y-10 sm:px-20 lg:space-y-0 lg:space-x-10 lg:flex-row xl:px-32'>
+                <div className='z-10 flex flex-col w-11/12 px-5 py-4 border-2 border-gray-400 shadow-md shadow-lg sm:w-9/12 bg-white-true'>
                     <div className='flex items-center justify-between w-full pb-3'>
                         <h3 className='font-heading text-black-heading'>Make Booking</h3>
-                        <aside className='px-3 py-1 rounded-md font-heading text-blue-primary bg-blue-shadowOne'>1</aside>
+                        <aside className='px-2 py-0 rounded-md sm:px-3 sm:py-1 font-heading text-blue-primary bg-blue-shadowOne'>1</aside>
                     </div>
                     <p className='text-sm '>Est penatibus pretium, ut mi. Pulvinar ante orci euismod lectus. </p>
                 </div>
-                <div className='z-10 flex flex-col w-4/12 px-5 py-4 border-2 border-gray-400 shadow-md shadow-lg bg-white-true'>
+                <div className='z-10 flex flex-col w-11/12 px-5 py-4 border-2 border-gray-400 shadow-md shadow-lg sm:w-9/12 bg-white-true'>
                     <div className='flex items-center justify-between w-full pb-3'>
                         <h3 className='font-heading text-black-heading'>Contract & Invoice</h3>
-                        <aside className='px-3 py-1 rounded-md font-heading text-blue-primary bg-blue-shadowOne'>2</aside>
+                        <aside className='px-2 py-0 rounded-md sm:px-3 sm:py-1 font-heading text-blue-primary bg-blue-shadowOne'>2</aside>
                     </div>
                     <p className='text-sm '>Est penatibus pretium, ut mi. Pulvinar ante orci euismod lectus. </p>
                 </div>
-                <div className='z-10 flex flex-col w-4/12 px-5 py-4 border-2 border-gray-400 shadow-md shadow-lg bg-white-true'>
+                <div className='z-10 flex flex-col w-11/12 px-5 py-4 border-2 border-gray-400 shadow-md shadow-lg sm:w-9/12 bg-white-true'>
                     <div className='flex items-center justify-between w-full pb-3'>
                         <h3 className='font-heading text-black-heading'>Payment</h3>
-                        <aside className='px-3 py-1 rounded-md font-heading text-blue-primary bg-blue-shadowOne'>3</aside>
+                        <aside className='px-2 py-0 rounded-md sm:px-3 sm:py-1 font-heading text-blue-primary bg-blue-shadowOne'>3</aside>
                     </div>
                     <p className='text-sm '>Est penatibus pretium, ut mi. Pulvinar ante orci euismod lectus. </p>
                 </div>
-                <svg className='absolute pr-32 text-gray-400 top-1/2 pl-28' viewBox="0 0 100 5" xmlns="http://www.w3.org/2000/svg">
-                    <line x1={0} y1={0} x2={100} y2={0} stroke="#B6B3B3" />
+                <svg className='absolute hidden pr-32 text-gray-400 lg:block lg:top-1/2 pl-28' viewBox="0 0 100 5" xmlns="http://www.w3.org/2000/svg">
+                    <line className='' x1={0} y1={0} x2={100} y2={0} stroke="#B6B3B3" />
+                    {/* <line className='inline-flex lg:hidden' x1={0} y1={0} x2={0} y2={100} stroke="#B6B3B3" /> */}
                 </svg>
+                <div className='absolute block lg:hidden left-1/2 min-h-sm'>
+                    <svg height={300} width={5}>
+                        <line x1={0} y1={0} x2={0} y2={300} style={{ strokeWidth: 5}} stroke="#B6B3B3" />
+                    </svg>
+
+                </div>
             </section>
         </div>
     )
