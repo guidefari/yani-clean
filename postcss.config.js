14272928e870836740c5c9d148adddc0227c41e2
GT Fari <guideg6@gmail.com>
2020-11-30 11:45:44 +0200

    fml

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // tailwindcss: {},
-    // autoprefixer: {},
-  ]
+  plugins: {
+    'tailwindcss': {},
+    'postcss-import': {},
+    'autoprefixer': {},
+  }
 }
