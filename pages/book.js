7eb087d8b6f6e2c23d5f8582e50b8789b7aaac76
GT Fari <guideg6@gmail.com>
2020-12-04 17:57:42 +0200

    button initial styling

--- pages/book.js
+++ pages/book.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Layout from '../components/layout';
 
 const book = () => {
     const [typeOfService, setTypeOfService] = useState('regular')
@@ -22,63 +23,83 @@ const book = () => {
 
     
     return (
-        <div className='container flex items-center justify-center min-h-xs'>
-            <form action="" className='flex flex-col space-y-10'>
-                <section id="typeOfServiceWrapper" className='flex flex-col'>
-                    <h1 className='text-2xl'>Type of Service</h1>
-                    <div className='flex space-x-10'>
-                        <button type="button" value='regular' onClick={() => setTypeOfService('regular')}>Regular</button>
-                        <button type="button" value='endOfTenancy' onClick={() => setTypeOfService('endOfTenancy')}>End of Tenancy</button>
-                    </div>
-                </section>
-                <section id="propertySizeWrapper" className='flex flex-col'>
-                    <h1 className='text-2xl'>Property Size</h1>
-                    <div className='flex space-x-10'>
-                        <button type="button" onClick={() => setPropertyType('studio')}>Studio</button>
-                        <button type="button" onClick={() => setPropertyType('notStudio')}>2+ Bedroom</button>
-                    </div>
-                    <div className='flex space-x-10'>
-                        <label>Bathrooms 
-                            <input onChange={(e) => setPropertySize({...propertySize, bath: Number(e.target.value)})} min={0} className='w-14' name="Bathrooms" type="number" />
-                        </label>
-                        <label>Bedrooms
-                            <input onChange={(e) => setPropertySize({...propertySize, bed: Number(e.target.value)})} min={0} className='w-14' name="Bedrooms" type="number" />
-                        </label>
-                    </div>
-                </section>
-                {(typeOfService === 'regular' ) && (<section id="serviceFrequencyWrapper" className='flex flex-col'>
-                    <h1 className='text-2xl'>Service Frequency</h1>
-                    <div className="mt-2">
-                        <div>
-                            <button type="button" onClick={() => setServiceFrequency('weekly')}>Weekly</button>
+        <Layout>
+
+            <div className='container flex flex-col items-center justify-center min-h-xs'>
+                <div className='flex items-center justify-center pt-4 pb-8'>
+                    <h1 className='text-xl md:text-3xl lg:text-5xl font-headingSecondary text-black-heading'>Make Booking</h1>
+                </div>
+                <form action="" className='flex flex-col max-w-sm px-3 py-5 space-y-10 lg:max-w-xl bg-white-true'>
+                    <section id="typeOfServiceWrapper" className='flex flex-col'>
+                        <div className='pb-5 space-y-5'>
+                            <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Type of Service</h3>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!</p>
                         </div>
-                        <div>
-                            <button type="button" onClick={() => setServiceFrequency('biweekly')}>BiWeekly</button>
+                        <div className='flex items-center justify-center space-x-10'>
+                            <button className='px-5 py-3 rounded-md font-headingSecondary bg-blue-tertiary focus:outline-none focus:ring-2 focus:ring-blue-primary focus:ring-opacity-50' type="button" value='regular' onClick={() => setTypeOfService('regular')}>Regular</button>
+                            <button className='px-5 py-3 rounded-md font-headingSecondary bg-blue-tertiary focus:outline-none focus:ring-2 focus:ring-blue-primary focus:ring-opacity-50' type="button" value='endOfTenancy' onClick={() => setTypeOfService('endOfTenancy')}>End of Tenancy</button>
                         </div>
-                        <div>
-                            <button type="button" onClick={() => setServiceFrequency('monthly')}>Monthly</button>
+                    </section>
+                    <section id="propertySizeWrapper" className='flex flex-col'>
+                        <div className='pb-5 space-y-5'>
+                            <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Property Size</h3>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!</p>
                         </div>
-                    </div>
-                </section>)}
-                <section id="addOnServicesWrapper" className='flex flex-col'>
-                    <h1 className='text-2xl'>Add on Services</h1>
-                    <div className="mt-2">
-                        <div>
-                            <button type="button" onClick={() => handleAddOnServices('washer')}>Washer</button>
+                        <div className='flex items-center justify-center space-x-10'>
+                            <button type="button" onClick={() => setPropertyType('studio')}>Studio</button>
+                            <button type="button" onClick={() => setPropertyType('notStudio')}>2+ Bedroom</button>
                         </div>
-                        <div>
-                            <button type="button" onClick={() => handleAddOnServices('stove')}>Stove</button>
+                        <div className='flex space-x-10'>
+                            <label>Bathrooms 
+                                <input onChange={(e) => setPropertySize({...propertySize, bath: Number(e.target.value)})} min={0} className='w-14' name="Bathrooms" type="number" />
+                            </label>
+                            <label>Bedrooms
+                                <input onChange={(e) => setPropertySize({...propertySize, bed: Number(e.target.value)})} min={0} className='w-14' name="Bedrooms" type="number" />
+                            </label>
                         </div>
-                        <div>
-                            <button type="button" onClick={() => handleAddOnServices('nhingi')}>Nhingi</button>
+                    </section>
+                    {(typeOfService === 'regular' ) && (
+                        <section id="serviceFrequencyWrapper" className='flex flex-col'>
+                            <div className='pb-5 space-y-5'>
+                                <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Service Frequency</h3>
+                                <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!</p>
+                            </div>
+                            <div className="flex items-center justify-center mt-2 space-x-10">
+                                <div>
+                                    <button type="button" onClick={() => setServiceFrequency('weekly')}>Weekly</button>
+                                </div>
+                                <div>
+                                    <button type="button" onClick={() => setServiceFrequency('biweekly')}>BiWeekly</button>
+                                </div>
+                                <div>
+                                    <button type="button" onClick={() => setServiceFrequency('monthly')}>Monthly</button>
+                                </div>
+                            </div>
+                        </section>
+                    )}
+                    <section id="addOnServicesWrapper" className='flex flex-col'>
+                        <div className='pb-5 space-y-5'>
+                            <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Add on Services</h3>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!</p>
                         </div>
-                        <div>
-                            <button type="button" onClick={() => handleAddOnServices('item')}>item here</button>
+                        <div className="flex items-center justify-center mt-2 space-x-10">
+                            <div>
+                                <button type="button" onClick={() => handleAddOnServices('washer')}>Washer</button>
+                            </div>
+                            <div>
+                                <button type="button" onClick={() => handleAddOnServices('stove')}>Stove</button>
+                            </div>
+                            <div>
+                                <button type="button" onClick={() => handleAddOnServices('nhingi')}>Nhingi</button>
+                            </div>
+                            <div>
+                                <button type="button" onClick={() => handleAddOnServices('item')}>item here</button>
+                            </div>
                         </div>
-                    </div>
-                </section>
-            </form>
-        </div>
+                    </section>
+                </form>
+            </div>
+        </Layout>
     )
 }
 
