d85f98ad8b0a6c033902905a84a969e73340c595
GT Fari <guideg6@gmail.com>
2020-12-21 21:43:19 +0200

    fix: hero component changes from ntax

--- components/Home-page/Hero.jsx
+++ components/Home-page/Hero.jsx
@@ -3,22 +3,22 @@ import Link from 'next/link'
 
 const Hero = () => {
     return (
-        <div className='relative flex items-center justify-center mx-3 sm:shadow sm:shadow-md sm:pt-14 min-h-sm md:bg-blue-300 sm:mt-6 md:mt-0 lg:py-10'>
+        <div className='relative flex items-center justify-center mx-3 sm:shadow sm:shadow-md sm:pt-14 min-h-sm md:pt-0 md:bg-blue-300 sm:mt-6 md:mt-0 lg:py-10'>
             <div className='absolute flex items-center justify-center md:static md:w-1/2'>
                 <div className='relative lg:pl-10'>
                     <img
                         src='/assets/hero.png'
                         title='Hero image'
                         alt='Lady shaking a rug'
-                        className='pb-5 rounded-none sm:h-full'
+                        className='inline-block pb-5 rounded-none sm:h-full md:h-72 lg:h-96 xl:h-full'
                     />
                     <div className='absolute top-0 left-0 w-full opacity-70 bg-gradient-to-br from-blue-primary to-blue-secondary manual-height md:hidden'/>
                 </div>
             </div>
             <div className='absolute my-auto md:w-1/2 md:static'>
                 <main className='flex flex-col items-end justify-center pt-2 pr-5 md:pr-0 lg:pr-9 xl:pr-10 lg:pt-8 xl:pt-16 text-white-primary'>
-                    <h1 className='pb-4 text-2xl text-right xl:pb-10 md:text-4xl lg:text-5xl xl:text-6xl font-heading md:text-black-heading'>Pristine surroundings,<br/>effortlessly</h1>
-                    <p className='max-w-xl pb-6 text-sm text-right md:text-base lg:text-xl md:max-w-4xl md:text-black-BoBody'>Our service let’s you enjoy an immaculate home, whilst you save on time and effort. We put a sparkling house a phone call away.</p>
+                    <h1 className='pb-4 text-2xl text-right xl:pb-10 md:text-3xl lg:text-4xl xl:text-5xl font-heading md:text-black-heading'>Pristine surroundings,<br/>effortlessly</h1>
+                    <p className='max-w-xl pb-6 text-sm text-right md:text-sm md:leading-4 lg:text-lg lg:leading-5 md:max-w-4xl md:text-black-BoBody'>Our service let’s you enjoy an immaculate home, whilst you save on time and effort. We put a sparkling house a phone call away.</p>
                     <Link href='/book'>
                         <button className="inline-flex items-center px-3 py-1 text-base font-semibold border-0 rounded text-white-primary bg-blue-primary focus:outline-none hover:bg-blue-secondary ">
                             Get Started
--- components/footer.jsx
+++ components/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="flex items-center justify-center py-8 bg-blue-900 text-white-true">
         <main className="flex flex-col items-center justify-between max-w-3xl p-4 mx-auto space-y-5 md:p-8">
           <img className='inline-block max-w-xs h-28' src="/assets/logo-twi.svg" alt=""/>
-          <p>ST JAMES HOUSE, PENDLETON, SALFORD, MANCHESTER</p>
+          <p>St James House, Pendleton, Salford, Manchester</p>
           <aside className='text-xs text-gray-400'>2020 © Yani Cleaning Services. All Rights Reserved.</aside>
         </main>
       </footer>
