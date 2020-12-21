fc307d6d20af532a80a73de6099f6bf78f4acd83
GT Fari <guideg6@gmail.com>
2020-12-21 23:16:39 +0200

    update: booking page responsive init

--- pages/book.js
+++ pages/book.js
@@ -46,7 +46,8 @@ const book = () => {
                 <div className='flex items-center justify-center pt-4 pb-8'>
                     <h1 className='text-xl md:text-3xl lg:text-5xl font-headingSecondary text-black-heading'>Make Booking</h1>
                 </div>
-                <form name="booking" method="POST" data-netlify="true" className='grid grid-cols-3 gap-4 '>
+                <form name="booking" method="POST" data-netlify="true" className='grid grid-cols-1 lg:grid-cols-3 lg:gap-4 '>
+                    <>
                         <input type="hidden" name="bot-field" />
                         <input type="hidden" name="form-name" defaultValue="booking" />
                         <input type="hidden" name='property-type' value={propertyType}/>
@@ -55,8 +56,8 @@ const book = () => {
                         <input type="hidden" name='type-of-service' value={typeOfService}/>
                         <input type="hidden" name='service-frequency' value={serviceFrequency}/>
                         <input type="hidden" name='addOnServices' value={addOnServices}/>
-
-                    <div id='detailsInput' className='max-w-sm col-span-2 px-8 py-10 mb-20 space-y-10 rounded-md lg:max-w-xl bg-white-true'>
+                    </>
+                    <div id='detailsInput' className='max-w-md px-8 py-10 space-y-10 rounded-md md:max-w-sm md:mb-20 md:col-span-2 lg:max-w-xl bg-white-true'>
                         <section id="typeOfServiceWrapper" className='flex flex-col'>
                             <div className='pb-5 space-y-5'>
                                 <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Type of Service</h3>
@@ -72,12 +73,12 @@ const book = () => {
                                 <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Property Size</h3>
                                 <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!</p>
                             </div>
-                                <div className='flex items-center justify-center pb-5 space-x-10 '>
+                                <div className='flex items-center justify-center space-x-10 '>
                                     <button className='form-btn' type="button" onClick={() => handlePropertyType('notStudio')}>2+ Bed</button>
                                     <button className='form-btn' type="button" onClick={() => handlePropertyType('studio')}>Studio</button>
                                 </div>
                             {(propertyType === 'notStudio') && (
-                                <div className='flex items-center justify-center space-x-10'>
+                                <div className='flex items-center justify-center pt-5 space-x-10'>
                                     <div>
                                         <label htmlFor="bedrooms" className="block text-sm font-medium text-gray-700">Bedrooms</label>
                                         <input 
@@ -109,7 +110,7 @@ const book = () => {
                                     <h3 className='text-lg md:text-2xl lg:text-4xl font-heading text-black-heading'>Service Frequency</h3>
                                     <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, itaque!</p>
                                 </div>
-                                <div className="flex items-center justify-center mt-2 space-x-10">
+                                <div className="flex items-center justify-center mt-2 space-x-5 sm:space-x-10">
                                     <div>
                                         <button className='form-btn' type="button" onClick={() => setServiceFrequency('Weekly')}>Weekly</button>
                                     </div>
@@ -205,7 +206,7 @@ const book = () => {
                             </div>
                         </section>
                     </div>
-                    <div id='detailsConfirm' className="sticky flex flex-col justify-between col-span-1 px-5 pt-10 pb-12 space-y-5 bg-white-secondary top-20 h-52">
+                    <div id='detailsConfirm' className="sticky flex flex-col justify-between px-5 pt-10 pb-12 space-y-5 md:col-span-1 bg-white-secondary top-20 h-52">
                         <section>
                             <h3 className='text-sm font-heading text-black-heading'>{(typeOfService === 'regular' ) ? `${serviceFrequency} Cleaning Plan` : `End of Tenancy Cleaning Plan`} </h3>
                             <div className='text-xs'>
@@ -220,7 +221,7 @@ const book = () => {
                                 </div>
                             </section>
                         )}
-                        <button type='submit' className="inline-flex items-center px-3 py-1 my-5 text-base font-semibold border-0 rounded text-blue-primary bg-blue-shadowOne focus:outline-none hover:bg-blue-shadowTwo ">
+                        <button type='submit' className="inline-flex items-center justify-center px-3 py-1 my-5 text-base font-semibold border-0 rounded text-blue-primary bg-blue-shadowOne focus:outline-none hover:bg-blue-shadowTwo ">
                             Confirm Booking
                         </button>
                     </div>
