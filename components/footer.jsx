523f8f89a5c96ea30bd621caae15d40ba64c188f
GT Fari <guideg6@gmail.com>
2020-12-22 00:26:38 +0200

    fix: footer responsive styling

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,19 @@
+import { Mail, PhoneCall } from "react-feather";
+
 function Footer() {
   return (
     <footer className="flex items-center justify-center py-8 bg-blue-900 text-white-true">
         <main className="flex flex-col items-center justify-between max-w-3xl p-4 mx-auto space-y-5 md:p-8">
-          <img className='inline-block max-w-xs h-28' src="/assets/logo-twi.svg" alt=""/>
-          <p>St James House, Pendleton, Salford, Manchester</p>
+          <img className='inline-block h-20 max-w-xs sm:h-28' src="/assets/logo-twi.svg" alt=""/>
+          <p className='text-sm sm:text-base'>St James House, Pendleton, Salford, Manchester</p>
+          <div className='flex space-x-5'>
+            <a href="tel:+442034413037">
+              <PhoneCall className='inline-block hover:text-yellow-primary'/>
+            </a>
+            <a href="mailto:info@yanicleaningservices.co.uk">
+              <Mail className='inline-block hover:text-yellow-primary'/>
+            </a>
+          </div>
           <aside className='text-xs text-gray-400'>2020 © Yani Cleaning Services. All Rights Reserved.</aside>
         </main>
       </footer>
