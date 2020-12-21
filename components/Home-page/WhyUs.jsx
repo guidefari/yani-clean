d7b4bb3c58b5df4bc70eea2f9d29bec8ac6d34ff
GT Fari <guideg6@gmail.com>
2020-12-21 21:27:00 +0200

    fix: why us svg pattern

--- components/Home-page/WhyUs.jsx
+++ components/Home-page/WhyUs.jsx
@@ -1,5 +1,4 @@
 import { Activity, Calendar, Heart, PlusCircle } from "react-feather"
-import Whyusdots from "../../public/assets/whyusdots";
 
 const Feature = () => {
     return (
@@ -7,7 +6,14 @@ const Feature = () => {
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
           <h2 className="max-w-lg mb-6 text-3xl leading-none tracking-tight font-heading text-black-heading sm:text-4xl md:mx-auto">
             <span className="relative inline-block">
-                <Whyusdots/>
+            <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-yellow-primary lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                <defs>
+                    <pattern id="07690130-d013-42bc-83f4-90de7ac68f76" x={0} y={0} width=".135" height=".30">
+                    <circle cx={1} cy={1} r=".7" />
+                    </pattern>
+                </defs>
+                <rect fill="url(#07690130-d013-42bc-83f4-90de7ac68f76)" width={52} height={24} />
+            </svg>
               <span className="relative">Why</span>
             </span>{' '}
             choose us
